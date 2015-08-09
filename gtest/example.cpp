0bebe37ac22a90c3fc4e6cb4c1c2fc6b6e65b461
paul-sh <paul.shmakov@gmail.com>
2015-08-09 17:02:06 +0300

    Added support for Google Test

---
+++ gtest/example.cpp
@@ -0,0 +1,27 @@
+#include <gtest/gtest.h>
+#include "teamcity_gtest.h"
+
+TEST(TestCase1, SuccessfulTest1) {
+    EXPECT_EQ(1, 1);
+}
+TEST(TestCase1, MultipleFailures) {
+    EXPECT_EQ(1, 2);
+    EXPECT_EQ(2, 3);
+    EXPECT_EQ(3, 4);
+}
+TEST(TestCase2, SuccessfulTest2) {
+    EXPECT_EQ(1, 1);
+}
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+
+    if (JetBrains::underTeamcity()) {
+        ::testing::TestEventListeners& listeners = ::testing::UnitTest::GetInstance()->listeners();
+        // Add unique flowId parameter if you want to run test processes in parallel
+        // See http://confluence.jetbrains.net/display/TCD6/Build+Script+Interaction+with+TeamCity#BuildScriptInteractionwithTeamCity-MessageFlowId
+        listeners.Append(new JetBrains::TeamcityGoogleTestEventListener());
+    }
+
+    return RUN_ALL_TESTS();
+}
