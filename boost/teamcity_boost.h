a488732c31d265d1fbdd70eef33646998cc3dca8
Leonid Shalupov <leonid@shalupov.com>
2017-03-07 00:49:23 +0100

    comment for https://github.com/JetBrains/teamcity-cpp/pull/19

--- boost/teamcity_boost.h
+++ boost/teamcity_boost.h
@@ -1,8 +1,12 @@
 #ifndef H_TEAMCITY_BOOST
 #define H_TEAMCITY_BOOST
 
-namespace jetbrains { namespace teamcity {
-    void TeamcityGlobalFixture();
-}}
+namespace jetbrains { 
+    namespace teamcity {
+        // Dummy method used to keep object file in case of static library linking
+        // See README.md and https://github.com/JetBrains/teamcity-cpp/pull/19
+        void TeamcityGlobalFixture();
+    }
+}
 
 #endif /* H_TEAMCITY_BOOST */
